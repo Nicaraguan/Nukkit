7044ac69b78044291e87d946f05e61eabe8364f3
Marcio Camilo Vargas Valle <tasxeno@gmail.com>
2019-10-16 12:18:35 +0200

    Change archievements texts
    Just change archievements text to cooler ones

--- src/main/java/cn/nukkit/Achievement.java
+++ src/main/java/cn/nukkit/Achievement.java
@@ -5,23 +5,23 @@ import cn.nukkit.utils.TextFormat;
 import java.util.HashMap;
 
 /**
- * Created by CreeperFace on 9. 11. 2016.
+ * Created by CreeperFace on 9. 11. 2016. Edited by Marci0
  */
 public class Achievement {
 
     public static final HashMap<String, Achievement> achievements = new HashMap<String, Achievement>() {
         {
-            put("mineWood", new Achievement("Getting Wood"));
-            put("buildWorkBench", new Achievement("Benchmarking", "mineWood"));
-            put("buildPickaxe", new Achievement("Time to Mine!", "buildWorkBench"));
-            put("buildFurnace", new Achievement("Hot Topic", "buildPickaxe"));
-            put("acquireIron", new Achievement("Acquire hardware", "buildFurnace"));
-            put("buildHoe", new Achievement("Time to Farm!", "buildWorkBench"));
-            put("makeBread", new Achievement("Bake Bread", "buildHoe"));
+            put("mineWood", new Achievement("Looks like he wants to destroy the eco-environement!"));
+            put("buildWorkBench", new Achievement("Time to craft!", "mineWood"));
+            put("buildPickaxe", new Achievement("It's dangerous to go alone", "buildWorkBench"));
+            put("buildFurnace", new Achievement("Hot hot hot hot hot!", "buildPickaxe"));
+            put("acquireIron", new Achievement("Ironification begun!", "buildFurnace"));
+            put("buildHoe", new Achievement("Ecology", "buildWorkBench"));
+            put("makeBread", new Achievement("Baguettes", "buildHoe"));
             put("bakeCake", new Achievement("The Lie", "buildHoe"));
-            put("buildBetterPickaxe", new Achievement("Getting an Upgrade", "buildPickaxe"));
-            put("buildSword", new Achievement("Time to Strike!", "buildWorkBench"));
-            put("diamonds", new Achievement("DIAMONDS!", "acquireIron"));
+            put("buildBetterPickaxe", new Achievement("Yea Better", "buildPickaxe"));
+            put("buildSword", new Achievement("Time to rekt some people", "buildWorkBench"));
+            put("diamonds", new Achievement("Yeaaaaaaah Diamondsssssssss!", "acquireIron"));
         }
     };
 
@@ -29,7 +29,7 @@ public class Achievement {
         if (!achievements.containsKey(achievementId)) {
             return false;
         }
-        String translation = Server.getInstance().getLanguage().translateString("chat.type.achievement", player.getDisplayName(), TextFormat.GREEN + achievements.get(achievementId).getMessage());
+        String translation = Server.getInstance().getLanguage().translateString("chat.type.achievement", player.getDisplayName(), TextFormat.GREEN + achievements.get(achievementId).getMessage() + TextFormat.RESET);
 
         if (Server.getInstance().getPropertyBoolean("announce-player-achievements", true)) {
             Server.getInstance().broadcastMessage(translation);
